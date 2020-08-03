f761424d4d345c5d9f1f0e88a96b9594618e2424
Tim Schumacher <timschumi@gmx.de>
2020-08-03 23:36:39 +0200

    Use the standard method signature for DevMsg
    It's a surprise tool that will help us later.

--- src/log.h
+++ src/log.h
@@ -1,18 +1,21 @@
 #include "tier0/dbg.h"
 
-#define DEV(fmt, ...)        DevMsg("[chttp] " fmt "\n", ##__VA_ARGS__)
+#define DEVL(l, fmt, ...)    DevMsg(l, "[chttp] " fmt "\n", ##__VA_ARGS__)
+#define DEV(fmt, ...)        DEVL(1, fmt, ##__VA_ARGS__)
 #define MSG(fmt, ...)        Msg("[chttp] " fmt "\n", ##__VA_ARGS__)
 #define WARN(fmt, ...)       Warning("[chttp] " fmt "\n", ##__VA_ARGS__)
 
 
 #ifdef NDEBUG
 
+#define DBGDEVL(fmt, ...)
 #define DBGDEV(fmt, ...)
 #define DBGMSG(fmt, ...)
 #define DBGWARN(fmt, ...)
 
 #else
 
+#define DBGDEVL(l, fmt, ...) DEVL(l, fmt, ##__VA_ARGS__)
 #define DBGDEV(fmt, ...)     DEV(fmt, ##__VA_ARGS__)
 #define DBGMSG(fmt, ...)     MSG(fmt, ##__VA_ARGS__)
 #define DBGWARN(fmt, ...)    WARN(fmt, ##__VA_ARGS__)
